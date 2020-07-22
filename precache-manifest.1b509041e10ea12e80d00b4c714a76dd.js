fe5f50534e94e977d7203918242747941d897115
liondor <johen.sylvestre@gmail.com>
2020-07-22 08:39:27 -0400

    Updates

---
+++ precache-manifest.1b509041e10ea12e80d00b4c714a76dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f84795432b2d04f562223c71ff20e46b",
+    "url": "/site-portfolio/index.html"
+  },
+  {
+    "revision": "f244b60768757ece3b4a",
+    "url": "/site-portfolio/static/css/main.41a4a909.chunk.css"
+  },
+  {
+    "revision": "f1af82a58c542925d0c9",
+    "url": "/site-portfolio/static/js/2.597dbe66.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/site-portfolio/static/js/2.597dbe66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f244b60768757ece3b4a",
+    "url": "/site-portfolio/static/js/main.080988c8.chunk.js"
+  },
+  {
+    "revision": "b461f44210dfd52be326",
+    "url": "/site-portfolio/static/js/runtime-main.41dc3cca.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/site-portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "f3db6cca6fa9bb4ad1a224131fbcefbc",
+    "url": "/site-portfolio/static/media/corona.f3db6cca.png"
+  },
+  {
+    "revision": "443fce3fdaa433ae080c6c9db9884beb",
+    "url": "/site-portfolio/static/media/techPortfolio.443fce3f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.27dd752709fac90c82c11512592d7df5.js"
+  "/site-portfolio/precache-manifest.1b509041e10ea12e80d00b4c714a76dd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
